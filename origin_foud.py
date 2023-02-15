b1c4f7176732a53b83a731dbbb33d5c6c9035996
it0m0k <mf22009@shibaura-it.ac.jp>
2023-02-16 01:07:20 +0900

    shibaura_RoadRunner_5,6版の座標に

--- origin_foud.py
+++ origin_foud.py
@@ -6,10 +6,11 @@ from pyproj import Proj
 lon_1=139.648076  #[deg.]
 lat_1=35.952118 #[deg.]
 
-#RoadRunnerの座標
+#RoadRunnerの座標(shibaura_RoadRunner_5,6の時)
+#0216時点で最新
 #芝浦工業大学交差点
-x_RoadRunner_1 = -91.102
-y_RoadRunner_1 = 95.103
+x_RoadRunner_1 = -74.807
+y_RoadRunner_1 = 95.659
 
 ##Compute UTM zone
 #"int": Extract only integer value
